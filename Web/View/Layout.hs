32f11dc1a8a1d045529c96fca5a1d0500916846d
Willy Unterkoefler <w.unterkoefler@gmail.com>
2022-03-13 14:46:13 -0400

    add favicons

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -13,6 +13,7 @@ import Application.Helper.View
 defaultLayout :: Html -> Html
 defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 <head>
+    {icons}
     {metaTags}
 
     {stylesheets}
@@ -71,4 +72,12 @@ metaTags = [hsx|
     <meta property="og:url" content="TODO"/>
     <meta property="og:description" content="TODO"/>
     {autoRefreshMeta}
+|]
+
+icons :: Html
+icons = [hsx|
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 |]
